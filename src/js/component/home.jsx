6125e65dc72e6c1a71264627377941e50842a250
Rui Silva <rsilva6420@gmail.com>
2023-06-09 15:57:06 +0000

    Final version

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,64 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	let [todo, setTodo] = useState('');
+	let [todoList, setTodoList] = useState([]);
+
+	const handleSubmit = (e) => {
+		if (e.key === "Enter" && todo != "") {
+			setTodoList([...todoList, todo]);
+			setTodo('');
+		}
+	}
+	
+	const deleteItem = (index) => {
+		let reduceList = [...todoList]
+		reduceList.splice(index, 1);
+		setTodoList(reduceList);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container pt-3 mt-4">
+			<div className="container w-100 text-center pt-3">
+			<h1>My ToDo List</h1>
+			</div>
+			<div className="mainContainer">
+				<div className="d-flex justify-content-center">
+					<input 
+						className="w-100 ps-3"
+						type="text"
+						placeholder="Enter a task"
+						value={todo}
+						onChange={(e) => {
+							setTodo(e.target.value);
+						}}
+						onKeyDown={handleSubmit}
+					/>
+				</div>
+				<div className="d-flex justify-content-center"> 
+					<ul className="w-100 p-0 m-0">
+						{todoList.length !== 0 && todoList.map((todoItem) => {
+							return <li>
+										<div className="d-flex task justify-content-between">
+											<div className="todoItem pb-1">{todoItem}</div>
+											<span><i class="far fa-times-circle fs-5 pb-1 pe-3 delete" onClick={deleteItem}></i></span>
+										</div>
+									</li>;
+						})}
+					</ul>
+				</div>
+				<div className="d-flex justify-content-left taskCounter">
+					<p>{todoList.length > 0 ? todoList.length : "" } items left</p>
+				</div>
+				<div className="d-flex taskCounter1">
+				</div>
+				<div className="d-flex taskCounter2">
+				</div>
+			</div>
+			
+			
 		</div>
 	);
 };
